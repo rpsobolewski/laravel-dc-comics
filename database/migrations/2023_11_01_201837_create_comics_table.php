8dec212ea6fa59ad0e1a3d9bc0ad89adb255aca1
rpsobolewski <rpsobolewski@gmail.com>
2023-11-01 21:41:49 +0100

    create migration
    created migration for comic

---
+++ database/migrations/2023_11_01_201837_create_comics_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        /*  */
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->longText('description')->nullable();
+            $table->longText('thumb')->nullable();
+            $table->tinyText('price');
+            $table->tinyText('target_age');
+            $table->tinyInteger('pages');
+            $table->string('type')->nullable();
+            $table->text('artists')->nullable();
+            $table->text('writers')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comics');
+    }
+};
